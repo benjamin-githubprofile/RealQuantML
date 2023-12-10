f00e85a983d40fe630a60ffc4166971eff826bb4
bennnnnn <145888032+benjamin-githubprofile@users.noreply.github.com>
2023-12-09 20:47:31 -0800

    Delete myproject directory

--- myproject/graphapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_file, name='upload_file'),
-    path('export_pdf/', views.export_pdf, name='export_pdf'),
-]
--- myproject/myproject/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path('graphapp/', include('graphapp.urls')),
-]
